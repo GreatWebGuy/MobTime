ce8ad38b793771baedc272372406f60106c8f22d
Jason Crow <jason.crow@officedepot.com>
2016-01-03 20:35:24 -0500

    Ditch enable/disable for now

--- src/main/java/com/greatwebguy/application/People.java
+++ src/main/java/com/greatwebguy/application/People.java
@@ -2,22 +2,13 @@ package com.greatwebguy.application;
 
 public class People {
 	private String name;
-	private boolean enabled = true;
 	
 	public People(String name) {
 		this.name = name;
 	}
 	
-	public void setEnabled(boolean value) {
-		this.enabled = value;
-	}
-	
 	public String getName() {
 		return name;
-	}
-	
-	public boolean isEnabled() {
-		return enabled;
 	}
 	
 	public String toString() {
