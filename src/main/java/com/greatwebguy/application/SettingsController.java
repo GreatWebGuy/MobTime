bbefe07612c6e073f5c42112d5dcf805232019df
Jason Crow <jason.crow@officedepot.com>
2016-01-03 20:33:19 -0500

    Add and remove Users

--- src/main/java/com/greatwebguy/application/SettingsController.java
+++ src/main/java/com/greatwebguy/application/SettingsController.java
@@ -15,6 +15,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 public class SettingsController implements Initializable {
 	@FXML // fx:id="timeSlider"
@@ -29,16 +31,24 @@ public class SettingsController implements Initializable {
 	@FXML // fx:id="addUser"
 	private Button addUser;
 
+	@FXML // fx:id="removeUser"
+	private Button removeUser;
+
+	@FXML // fx:id="nextUser"
+	private Button nextUser;
+
 	@FXML // fx:id="userList"
 	private ListView<People> userList;
-	
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		assert timeSlider != null : "fx:id=\"timeSlider\" was not injected: check your FXML file 'application.fxml'.";
 		assert timeInput != null : "fx:id=\"timeInput\" was not injected: check your FXML file 'application.fxml'.";
+		assert addUser != null : "fx:id=\"addUser\" was not injected: check your FXML file 'application.fxml'.";
+		assert removeUser != null : "fx:id=\"removeUser\" was not injected: check your FXML file 'application.fxml'.";
+		assert nextUser != null : "fx:id=\"nextUser\" was not injected: check your FXML file 'application.fxml'.";
 
-		timeInput.setText(String.valueOf(Settings.instance().getStartTime()));
+		timeInput.setText(Settings.instance().getStartTime() + "");
 		timeSlider.setValue(Settings.instance().getStartTime());
 		timeSlider.requestFocus();
 		timeSlider.valueProperty().addListener(new ChangeListener<Number>() {
@@ -49,17 +59,49 @@ public class SettingsController implements Initializable {
 				Settings.instance().setStartTime(value);
 			}
 		});
-		
+
 		userList.setItems(Settings.instance().users);
-		
-		addUser.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-            	if(StringUtils.isNotBlank(userInput.getText())) {
-            		Settings.instance().users.add(new People(userInput.getText()));
-            	}
-            }
+
+		userInput.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+				if (event.getCode() == KeyCode.ENTER) {
+					addUser();
+				}
+			}
 		});
+
+		addUser.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				addUser();
+			}
+		});
+
+		removeUser.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				int selectedIdx = userList.getSelectionModel().getSelectedIndex();
+				if (selectedIdx != -1) {
+					userList.getItems().remove(selectedIdx);
+				}
+			}
+		});
+
+		nextUser.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+
+			}
+		});
+
+	}
+
+	private void addUser() {
+		if (StringUtils.isNotBlank(userInput.getText())) {
+			Settings.instance().users.add(new People(userInput.getText()));
+			userInput.clear();
+		}
 	}
 
 }
