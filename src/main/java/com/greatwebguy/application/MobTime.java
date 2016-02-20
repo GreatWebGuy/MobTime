89bc850e38e39586cdd30c85ca6cdb44a6d13295
Jason Crow <jason.crow@officedepot.com>
2016-02-20 11:47:36 -0500

    Fix icons so they're displayed without internet access

--- src/main/java/com/greatwebguy/application/MobTime.java
+++ src/main/java/com/greatwebguy/application/MobTime.java
@@ -1,5 +1,11 @@
 package com.greatwebguy.application;
 
+import java.io.InputStream;
+
+import org.controlsfx.glyphfont.FontAwesome;
+import org.controlsfx.glyphfont.GlyphFont;
+import org.controlsfx.glyphfont.GlyphFontRegistry;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -12,6 +18,9 @@ public class MobTime extends Application {
 	@Override
 	public void start(Stage stage) {
 		try {
+			InputStream is = getClass().getResourceAsStream("fontawesome-webfont.ttf");
+			GlyphFont fa = new FontAwesome(is);
+			GlyphFontRegistry.register(fa);
 			Parent root = FXMLLoader.load(getClass().getResource("application.fxml"));
 			stage.setTitle("MobTime");
 			stage.getIcons().add(new Image(getClass().getResourceAsStream("icon.png")));
