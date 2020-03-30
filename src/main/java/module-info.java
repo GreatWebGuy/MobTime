0470bd83d14e393f726f8071789032b0acae2005
Jason Crow <jacrow@totalwine.com>
2020-03-30 08:43:54 -0400

    Set up module-info and exports in maven - however doesn't run anymore - but trying to figure out how to package

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module MobTime {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires org.controlsfx.controls;
+    requires org.apache.commons.lang3;
+    requires commons.io;
+}
